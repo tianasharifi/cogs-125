79193d122bb3bb7f6a762ec18b56ac9f7bb0e3f8
Seshu <seshumallina@gmail.com>
2024-04-30 12:40:19 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+         Tiana Sharifi's Portfolio 
+        </h1>
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
